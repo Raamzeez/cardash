74dc332a7320daa79069c0eeaeb44ff8f86f9cc1
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2024-09-08 19:14:43 -0400

    Added button to go to garage in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ import Navbar from "@/components/Navbar";
 import Overview from "@/components/Overview";
 import TirePressureCard from "@/components/InfoCard";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <p className="text-2xl">Home Page</p>
+      <p className="text-2xl mt-10">Home Page</p>
+      <button className="mt-24 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href={`/garage`}>Click to Go to Garage</Link>
+      </button>
     </main>
   );
 }
